8f816c12be5ec4298861497fd56d9e8701be2bff
Adarsh Hari Lohar <117507791+adarshhlohar@users.noreply.github.com>
2023-12-04 15:57:40 +0530

    We where change the name of  function And readable

--- src/main/java/com/bridgelabz/EmailValidation/EmailValidation.java
+++ src/main/java/com/bridgelabz/EmailValidation/EmailValidation.java
@@ -5,64 +5,85 @@ import java.util.regex.Pattern;
 
 public class EmailValidation {
 	// Validate email must be contain the @ symbol
-	//1. abc – must contains “@” symbol
-	public boolean validateEmail(String email) {
+	/*
+	 * It will Accept The email As String And the pattern will will matches with
+	 * that String If the String will Matches it will return true or it will return
+	 * false
+	 */
+	// 1. abc – must contains “@” symbol
+	public boolean validateEmailMustContainSpecSymbol(String email) {
 		Pattern p = Pattern.compile("^[a-zA-Z]+@[a-zA-Z]+\\.com");
 		Matcher m = p.matcher(email);
-		if(m.matches()) {
+		if (m.matches()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
-	//test2- abc@.com.my – tld can not start with dot “.”
-	public boolean validateEmail2(String email) {
+
+	/*
+	 * It will Accept The email As String And the pattern will will matches with
+	 * that String If the String will Matches it will return true or it will return
+	 * false
+	 */
+	// test2- abc@.com.my – tld can not start with dot “.”
+	public boolean validateEmailTLDCannotStartWithDot(String email) {
 		Pattern p = Pattern.compile("^[a-zA-Z0-9]+@[a-zA-Z]+\\.com");
 		Matcher m = p.matcher(email);
-		if(m.matches()) {
+		if (m.matches()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
-	//3. abc123@gmail.a – “.a” is not a valid tld, last tld must contains at least two
-	//characters
-	public boolean validateEmail3(String email) {
-		Pattern p = Pattern.compile("^[a-zA-Z0-9]+@[a-zA-Z]+\\.[a-z]{2,}");
+
+	// 3. abc123@gmail.a – “.a” is not a valid tld, last tld must contains at least
+	// two characters
+	/*
+	 * It will Accept The email As String And the pattern will will matches with
+	 * that String If the String will Matches it will return true or it will return
+	 * false
+	 */
+	public boolean validateEmailMustBeEndsWithTwoChar(String email) {
+		Pattern p = Pattern.compile("^[a-zA-Z0-9]+@[a-zA-Z]+\\.[a-z]{3,}$");
 		Matcher m = p.matcher(email);
-		if(m.matches()) {
+		if (m.matches()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
-	//6. .abc@abc.com – email’s 1st character can not start with “.”
-	public boolean validateEmail4(String email) {
-		Pattern p = Pattern.compile("^[^\\.][a-zA-Z0-9]+@[a-zA-Z]+\\.[a-z]{2,}");
+
+	/*
+	 * It will Accept The email As String And the pattern will will matches with
+	 * that String If the String will Matches it will return true or it will return
+	 * false
+	 */
+	// 6. .abc@abc.com – email’s 1st character can not start with “.”
+	public boolean validEmailFirstCharCannotStartWithDot(String email) {
+		Pattern p = Pattern.compile("^[^\\.][a-zA-Z0-9]+@[a-zA-Z]+\\.[a-z]{2,}$");
 		Matcher m = p.matcher(email);
-		if(m.matches()) {
+		if (m.matches()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
-	//7. abc()*@gmail.com – email’s is only allow character, digit, underscore and dash
-		public boolean validateEmail5(String email) {
-			Pattern p = Pattern.compile("^[^\\.][a-zA-Z0-9-_]+@[a-zA-Z0-9-_]+\\.[a-z]{2,}");
-			Matcher m = p.matcher(email);
-			if(m.matches()) {
-				return true;
-			}
-			else {
-				return false;
-			}
+
+	/*
+	 * It will Accept The email As String And the pattern will will matches with
+	 * that String If the String will Matches it will return true or it will return
+	 * false
+	 */
+	// 7. abc()*@gmail.com – email’s is only allow character, digit, underscore and
+	// dash
+	public boolean validateEmailCanAllowOnlyCharDigitUnderscoreDash(String email) {
+		Pattern p = Pattern.compile("^[^\\.][a-zA-Z0-9]+[_-][a-zA-Z0-9]+@[a-zA-Z0-9-_]+\\.[a-z]{2,}$");
+		Matcher m = p.matcher(email);
+		if (m.matches()) {
+			return true;
+		} else {
+			return false;
 		}
+	}
 }
--- src/test/java/com/bridgelabz/EmailValidation/EmailValidationTest.java
+++ src/test/java/com/bridgelabz/EmailValidation/EmailValidationTest.java
@@ -1,56 +1,117 @@
 package com.bridgelabz.EmailValidation;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class EmailValidationTest {
-	
+
 	EmailValidation emailValidation;
-	
+
 	@Before
 	public void setUp() {
 		emailValidation = new EmailValidation();
 	}
-	
-	//test1
-	//1. abc@yahoo.com Valid for this one
-	//1. abc – must contains “@” symbol
+
+	// test1
+	// 1. abc@yahoo.com Valid for this one
+	// 1. abc – must contains “@” symbol
+
+	/*
+	 * In this the test method if the given email is validate correctly the it will
+	 * pass the test case true
+	 */
 	@Test
-	public void test() {
-//		assertTrue(emailValidation.validateEmail("abc@yahoo.com"));
-//		assertTrue(emailValidation.validateEmail("abcgmail.com"));
+	public void givenEmailMustContainReturnTrue() {
+		assertTrue(emailValidation.validateEmailMustContainSpecSymbol("abc@yahoo.com"));
 	}
-	
-	//abc-100@yahoo.com this is valid
-	//tld can not start with dot “.”
+
+	/*
+	 * else this the test method if the given email is invalidate then it will pass
+	 * the test case as true
+	 */
 	@Test
-	public void test2() {
-//		assertTrue(emailValidation.validateEmail2("abcXS100@yahoo.com"));
-//		assertTrue(emailValidation.validateEmail2(".abcgmail.com"));
+	public void givenEmailShouldNotContainReturnFalse() {
+		assertFalse(emailValidation.validateEmailMustContainSpecSymbol("abcgmail.com"));
 	}
-	
-	//the email should can not end with only one char it should have min 2 char
+
+	// abc-100@yahoo.com this is valid
+	// tld can not start with dot “.”
+	/*
+	 * In this the test method if the given email is validate correctly the it will
+	 * pass the test case true
+	 */
 	@Test
-	public void test3() {
-//		assertTrue(emailValidation.validateEmail3("abc100@yahoo.com"));
-//		assertTrue(emailValidation.validateEmail("abc@gmail.co"));
+	public void givenEmailTLDCannotStartWithDotReturnTrue() {
+		assertTrue(emailValidation.validateEmailTLDCannotStartWithDot("abcXS100@yahoo.com"));
 	}
-	
-	//6. .abc@abc.com – email’s 1st character can not start with “.”
+
+	/*
+	 * else this the test method if the given email is invalidate then it will pass
+	 * the test case as true
+	 */
 	@Test
-	public void test4() {
-//		assertTrue(emailValidation.validateEmail4("adarsh123@gmai.com"));
-//		assertTrue(emailValidation.validateEmail4(".adarsh123@gmail.com"));
+	public void givenEmailTLDCanStartWithDotReturnFalse() {
+		assertFalse(emailValidation.validateEmailTLDCannotStartWithDot(".abcgmail.com"));
 	}
-	
-	
-	//7. abc()*@gmail.com – email’s is only allow character, digit, underscore and dash
+
+	/*
+	 * In this the test method if the given email is validate correctly the it will
+	 * pass the test case true
+	 */
+//	3. abc123@gmail.a – “.a” is not a valid tld, last tld must contains at least two characters
 	@Test
-	public void test5() {
-		//it will work for the both test cases
-		assertTrue(emailValidation.validateEmail5("adarsh-123@gmai.com"));
-		assertTrue(emailValidation.validateEmail4("adarsh123@gmail.com"));
+	public void givenEmailCannotEndsWithMinTwoCharReturnTrue() {
+		assertTrue(emailValidation.validateEmailTLDCannotStartWithDot("abc100@yahoo.com"));
+	}
+
+	/*
+	 * else this the test method if the given email is invalidate then it will pass
+	 * the test case as true
+	 */
+	@Test
+	public void givenEmailCannotEndsWithMinTwoCharReturnFalse() {
+		assertFalse(emailValidation.validateEmailTLDCannotStartWithDot("acas@gmail.co"));
+	}
+
+	/*
+	 * In this the test method if the given email is validate correctly the it will
+	 * pass the test case true
+	 */
+	// 6. .abc@abc.com – email’s 1st character can not start with “.”
+	@Test
+	public void givenEmailCannotStartWithDotReturnTrue() {
+		assertTrue(emailValidation.validEmailFirstCharCannotStartWithDot("adarsh123@gmai.com"));
+	}
+
+	/*
+	 * else this the test method if the given email is invalidate then it will pass
+	 * the test case as true
+	 */
+	@Test
+	public void givenEmailCanStartWithDotReturnFalse() {
+		assertFalse(emailValidation.validEmailFirstCharCannotStartWithDot(".adarsh123@gmail.com"));
+	}
+
+	/*
+	 * In this the test method if the given email is validate correctly the it will
+	 * pass the test case true
+	 */
+	// 7. abc()*@gmail.com – email’s is only allow character, digit, underscore and
+	// dash
+	@Test
+	public void givenEmailShouldContainCharDigitUnderDashReturnTrue() {
+		assertTrue(emailValidation.validateEmailCanAllowOnlyCharDigitUnderscoreDash("adarsh-123@gmai.com"));
+	}
+
+	/*
+	 * else this the test method if the given email is invalidate then it will pass
+	 * the test case as true
+	 */
+	@Test
+	public void givenEmailShouldNotContainCharDigitUnderDashReturnFalse() {
+		assertFalse(emailValidation.validateEmailCanAllowOnlyCharDigitUnderscoreDash("adarsh123@gmail.com"));
 	}
 }
